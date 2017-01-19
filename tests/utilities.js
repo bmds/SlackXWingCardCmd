cb6f4020f590c0cb93bfab7a59d3b048d44b7f9e
Barney Scott <barney.scott@codecomputerlove.com>
2017-01-19 15:46:41 +0000

    test: Add the Ava test framework and tests for the utilities file

---
+++ tests/utilities.js
@@ -0,0 +1,54 @@
+import test from 'ava';
+import {checkEquality, arrayToObject, cardId} from '../lib/utilities';
+
+// Card ID tests
+test('card id pilot', async t => {
+	let response = cardId({
+		id:   12,
+		slot: 'pilot'
+	});
+
+	t.is(response, '12p');
+});
+
+test('card id condition', async t => {
+	let response = cardId({
+		id:   0,
+		slot: 'condition'
+	});
+
+	t.is(response, '0c');
+});
+
+test('card id modification', async t => {
+	let response = cardId({
+		id:   256,
+		slot: 'modification'
+	});
+
+	t.is(response, '256u');
+});
+
+test('card id unknown', async t => {
+	let response = cardId({
+		id:   10,
+		slot: 'hat'
+	});
+
+	t.is(response, '10u');
+});
+
+test('array to object', async t => {
+	let baseArr = [
+		{ id: 'one',text: 'cats' },
+		{ id: 'two',text: 'in' },
+		{ id: 'three',text: 'hats' }
+	];
+	let response = arrayToObject(baseArr, (element) => element.id);
+
+	t.deepEqual(response, {
+		one:   { id: 'one',text: 'cats' },
+		two:   { id: 'two',text: 'in' },
+		three: { id: 'three',text: 'hats' }
+	});
+});
