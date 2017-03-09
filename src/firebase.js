a9aa29dc4cb1c49909c8e75a904c583a46bb3210
Barney Scott <barney.scott@codecomputerlove.com>
2017-03-09 15:50:34 +0000

    fix: Look for the correct environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -32,15 +32,15 @@ function getCredentials() {
 	/*eslint-disable camelcase */
 	return {
 		type:                        'service_account',
-		project_id:                  process.env.PROJECT_ID,
-		private_key_id:              process.env.KEY_ID,
-		private_key:                 process.env.PRIVATE_KEY,
-		client_email:                process.env.CLIENT_EMAIL,
-		client_id:                   process.env.CLIENT_ID,
+		project_id:                  process.env.FIRE_PROJECT_ID,
+		private_key_id:              process.env.FIRE_KEY_ID,
+		private_key:                 process.env.FIRE_PRIVATE_KEY,
+		client_email:                process.env.FIRE_CLIENT_EMAIL,
+		client_id:                   process.env.FIRE_CLIENT_ID,
 		auth_uri:                    'https://accounts.google.com/o/oauth2/auth',
 		token_uri:                   'https://accounts.google.com/o/oauth2/token',
 		auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-		client_x509_cert_url:        'https://www.googleapis.com/robot/v1/metadata/x509/' + process.env.CLIENT_EMAIL.replace('@', '%40')
+		client_x509_cert_url:        'https://www.googleapis.com/robot/v1/metadata/x509/' + process.env.FIRE_CLIENT_EMAIL.replace('@', '%40')
 	};
 	/*eslint-enable camelcase */
 }
@@ -48,7 +48,7 @@ function getCredentials() {
 function setupConnection() {
 	let app = firebase.initializeApp({
 		credential:  firebase.credential.cert(getCredentials()),
-		databaseURL: `https://${process.env.PROJECT_ID}.firebaseio.com`
+		databaseURL: `https://${process.env.FIRE_PROJECT_ID}.firebaseio.com`
 	});
 
 	fbDb = app.database();
